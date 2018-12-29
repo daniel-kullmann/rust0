43dabd42715a5d731c29c8d4f81bca43c88bafdf
Daniel Kullmann <daniel.kullmann@gmx.de>
2018-12-29 21:51:21 +0100

    Implements /api/gpx/get/..

--- src/api/gpx.rs
+++ src/api/gpx.rs
@@ -1,13 +1,40 @@
 use hyper::{Body, Response};
 use serde_json;
-use std::fs::read_dir;
+use std::fs::{File, read_dir};
+use std::io::prelude::*;
 
 use crate::state::State;
 
 pub fn serve_gpx(uri: &String, state: &State) -> Response<Body> {
-    if uri.starts_with("/api/gpx/get") {
-        // TODO finish code
-        Response::new(Body::from("gpx get"))
+    if uri.starts_with("/api/gpx/get/") {
+        let file_name = &uri[13..];
+        let full_file = format!("{}/{}", &state.config.gpx_base, file_name);
+        println!("{:?}", full_file);
+        let fh = File::open(full_file);
+        match fh {
+            Err(why) => {
+                println!("{:?}", why);
+                let mut response = Response::builder();
+                response.status(StatusCode::NOT_FOUND);
+                Response::new(Body::from(""))
+            },
+            Ok(mut fh) => {
+                let mut content = String::new();
+                match fh.read_to_string(&mut content) {
+                    Ok(_) => {
+                        let mut response = Response::builder();
+                        response.header("Content-Type", "text/xml").status(StatusCode::OK);
+                        response.body(Body::from(content)).unwrap()
+                    },
+                    Err(why) => {
+                        println!("{:?}", why);
+                        let mut response = Response::builder();
+                        response.status(StatusCode::NOT_FOUND);
+                        Response::new(Body::from(""))
+                    }
+                }
+            }
+        }
     } else if uri.starts_with("/api/gpx/save") {
         // TODO finish code
         Response::new(Body::from("gpx save"))
