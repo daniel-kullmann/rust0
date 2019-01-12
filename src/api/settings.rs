21c291e9ce45d429abc70e41c452db3ac123149b
Daniel Kullmann <daniel.kullmann@gmx.de>
2019-01-12 10:42:12 +0000

    Fixed conversion from json Value to string

--- src/api/settings.rs
+++ src/api/settings.rs
@@ -38,7 +38,17 @@ pub fn serve_set_all_settings(req: &mut Request, state: &State) -> IronResult<Re
             match body {
                 serde_json::Value::Object(map) => {
                     for (key, value) in &map {
-                        state.connection.execute("REPLACE INTO setting (name, value) VALUES (?, ?)",&[key, &value.to_string().as_str()]).unwrap();
+                        let value: String = match value {
+                            serde_json::Value::String(val) => val.clone(),
+                            serde_json::Value::Number(val) => val.to_string(),
+                            serde_json::Value::Bool(true) => String::from("1"),
+                            serde_json::Value::Bool(false) => String::from("0"),
+                            serde_json::Value::Null => String::from(""), // TODO: rusqlite::types::Null,
+                            serde_json::Value::Array(_) => panic!(format!("can't store an object in database: {:?}", value)),
+                            serde_json::Value::Object(_) => panic!(format!("can't store an object in database: {:?}", value)),
+                        };
+                        let sql = "REPLACE INTO setting (name, value) VALUES (?, ?)";
+                        state.connection.execute(sql,&[key, &value.as_str()]).unwrap();
                     }
                 }
                 _ => ()
