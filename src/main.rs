064bdc680ca0ff95fb20908aa401d8679bdcac7c
Daniel Kullmann <daniel.kullmann@gmx.de>
2019-01-12 14:54:19 +0000

    Refactor: Moves more code to the library..

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,16 @@
 extern crate iron;
-extern crate mime_guess;
 extern crate r2d2_sqlite;
 extern crate rust0;
 
-use iron::mime::Mime;
 use iron::prelude::*;
-use iron::status;
-use mime_guess::guess_mime_type;
 use std::ops::Deref;
 use std::sync::Arc;
 use r2d2_sqlite::SqliteConnectionManager;
 
 use rust0::config::get_config;
-use rust0::api::gpx::serve_gpx;
-use rust0::api::settings::{serve_get_all_settings, serve_set_all_settings};
+use rust0::server::serve;
 use rust0::state::State;
-use rust0::tiles::serve_tile;
-use rust0::util::handle_error;
-use rust0::files::file_content;
 
-
-fn serve(req: &mut Request, state: &State) -> IronResult<Response> {
-    let uri = "/".to_owned() + &req.url.path().join(&"/");
-    if uri.starts_with("/tiles") {
-        serve_tile(&uri, state)
-    } else if uri.starts_with("/api/gpx/") {
-        serve_gpx(&req, &uri, state)
-    } else if uri.starts_with("/api/settings/set_all_settings/") {
-        serve_set_all_settings(req, state)
-    } else if uri.starts_with("/api/settings/") {
-        serve_get_all_settings(state)
-    } else {
-        let static_file = file_content(&uri);
-        match static_file {
-            Some(content) => {
-                let mime_type = guess_mime_type(&uri).to_string();
-                let content_type = mime_type.parse::<Mime>().expect("Failed to parse content type");
-                println!("Serving static file {} as {}", uri, mime_type);
-                Ok(Response::with((content_type, status::Ok, content)))
-            }
-            None => handle_error(status::NotFound, &""),
-        }
-    }
-}
 
 fn main() {
 
---
+++ src/server.rs
@@ -0,0 +1,40 @@
+use iron::mime::Mime;
+use iron::prelude::*;
+use iron::status;
+use mime_guess::guess_mime_type;
+
+use crate::api::gpx::serve_gpx;
+use crate::api::settings::{serve_get_all_settings, serve_set_all_settings};
+use crate::files::file_content;
+use crate::state::State;
+use crate::tiles::serve_tile;
+use crate::util::handle_error;
+
+fn serve_static_content(uri: &String) -> IronResult<Response> {
+    let static_file = file_content(&uri);
+    match static_file {
+        Some(content) => {
+            let mime_type = guess_mime_type(&uri).to_string();
+            let content_type = mime_type.parse::<Mime>().expect("Failed to parse content type");
+            println!("Serving static file {} as {}", uri, mime_type);
+            Ok(Response::with((content_type, status::Ok, content)))
+        }
+        None => handle_error(status::NotFound, &""),
+    }
+}
+
+pub fn serve(req: &mut Request, state: &State) -> IronResult<Response> {
+    let uri = "/".to_owned() + &req.url.path().join(&"/");
+    if uri.starts_with("/tiles") {
+        serve_tile(&uri, state)
+    } else if uri.starts_with("/api/gpx/") {
+        serve_gpx(&req, &uri, state)
+    } else if uri.starts_with("/api/settings/set_all_settings/") {
+        serve_set_all_settings(req, state)
+    } else if uri.starts_with("/api/settings/") {
+        serve_get_all_settings(state)
+    } else {
+        serve_static_content(&uri)
+    }
+}
+
