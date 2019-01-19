8a16bc8e8398fa77a0990097dcfcc4bec3b6587f
Daniel Kullmann <daniel.kullmann@gmx.de>
2019-01-19 17:27:42 +0000

    Fixes problem that tragtre directories weren't created

--- src/tiles.rs
+++ src/tiles.rs
@@ -11,12 +11,13 @@ use crate::util::handle_error;
 
 pub fn serve_tile(uri: &String, state: &State) -> IronResult<Response>
 {
-    match create_dir_all(Path::new(state.config.tile_base.as_str())) {
+    let rest: String = uri.chars().skip(7).collect();
+    let full_file = Path::new(&state.config.tile_base).join(rest);
+    let parent_dir = full_file.parent().unwrap();
+    match create_dir_all(parent_dir) {
         Ok(_) => (),
         Err(e) => println!("Error: {}", e)
-    }
-    let rest: String = uri.chars().skip(7).collect();
-    let full_file = format!("{}/{}", state.config.tile_base, rest);
+    };
     let len = metadata(&full_file).map(|v| v.len()).unwrap_or(0);
     let fh = File::open(&full_file);
     match (fh, len) {
@@ -25,7 +26,7 @@ pub fn serve_tile(uri: &String, state: &State) -> IronResult<Response>
             match file.read_to_end(&mut contents) {
                 Err(why) => handle_error(status::NotFound, &why),
                 Ok(_) => {
-                    println!("INFO: Served existing {}", full_file);
+                    println!("INFO: Served existing {}", full_file.to_str().unwrap());
                     let content_type = "image/png".parse::<Mime>().expect("Failed to parse content type");
                     Ok(Response::with((content_type, status::Ok, contents)))
                 }
@@ -45,7 +46,7 @@ pub fn serve_tile(uri: &String, state: &State) -> IronResult<Response>
 
                             let copy_to_result = response.copy_to(&mut buf);
                             match File::create(&full_file) {
-                                Err(why) => println!("ERROR: could not create tile file {}: {:?}", full_file, why),
+                                Err(why) => println!("ERROR: could not create tile file {}: {:?}", full_file.to_str().unwrap(), why),
                                 Ok(mut file) => {
                                     match file.write_all(&buf[..]) {
                                         Err(why) => println!("ERROR: could not save tile file: {:?}", why),
@@ -58,7 +59,7 @@ pub fn serve_tile(uri: &String, state: &State) -> IronResult<Response>
                             match copy_to_result {
                                 Err(why) => handle_error(status::NotFound, &why),
                                 Ok(_) => {
-                                    println!("INFO: Served fetched {}", full_file);
+                                    println!("INFO: Served fetched {}", full_file.to_str().unwrap());
                                     let content_type = "image/png".parse::<Mime>().expect("Failed to parse content type");
                                     Ok(Response::with((content_type, status::Ok, buf)))
                                 }
